d40a002b4a171450e56b4dd689e90a3797e4adaa
박진영 <tear4321@cau.ac.kr>
2024-08-29 18:29:44 +0900

    UI 끄적

---
+++ Assets/JYP/Scripts/Challenge/ChallengeInfo.cs
@@ -0,0 +1,21 @@
+﻿    public enum ChallengeType
+    {
+        A,
+        B,
+    }
+    
+    public enum ChallengeStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed,
+    }
+    
+
+    public class ChallengeInfo
+    {
+        public string name;
+        public ChallengeType type;
+        public string reward;
+        public ChallengeStatus status;
+    }
---
+++ Assets/JYP/Scripts/Data/Remote/ChallengeApi.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class ChallengeApi
+{
+    private static readonly string BASE_URL = "https://api.example.com";
+    
+    public static List<ChallengeInfo> GetChallengeListByUserId(string userId)
+    {
+        
+        string url = $"{BASE_URL}/challenges/{userId}";
+        return HttpHelper.Get(url);
+    }
+
+}
