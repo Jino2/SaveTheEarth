e5966a0814e57be4b7d71fafc19a6f324a56ac8f
hzkkong <hz0227798@gmail.com>
2024-08-29 09:04:09 +0900

    무브, 카메라 팔로우까지
    카메라 컨트롤은 아직

---
+++ Assets/HyeJi/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTarget;
+    public Transform lookAtTarget;
+    public bool hasDelay = false;
+    public float delaySpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lookAtTarget != null)
+        {
+            if (hasDelay)
+            {
+                // 살짝 느리게 쫓아간다
+                transform.position = Vector3.Lerp(transform.position, followTarget.position, Time.deltaTime * delaySpeed);
+            }
+            else
+            {
+                transform.position = followTarget.position;
+            }
+        }
+
+        if (lookAtTarget != null)
+        {
+            transform.rotation = lookAtTarget.rotation;
+        }
+    }
+}
---
+++ Assets/HyeJi/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : PlayerStateBase
+{
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 dir = new Vector3(h, 0, v);
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
+    }
+}
