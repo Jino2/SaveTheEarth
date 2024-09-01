a151911c0136353039f17e86abc9d64aa9010d0d
hzkkong <khj0227798@gmail.com>
2024-09-01 09:39:38 +0900

    0831 버츄얼 카메라 아직 수정완료X

--- Assets/HyeJi/Scripts/ThirdPersonCamera.cs
+++ Assets/HyeJi/Scripts/ThirdPersonCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
@@ -14,39 +15,55 @@ public class ThirdPersonCamera : MonoBehaviour
     // 플레이어 이동 입력 결과값 저장
     private Vector3 moveInput;
 
+
+    public CinemachineVirtualCamera virtualCamera;
+    public float rotationSpeed = 1f;
+    private Transform cameraTransform;
+    private CinemachineTransposer transposer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.position;     
+        cameraTransform = virtualCamera.VirtualCameraGameObject.transform;
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        //offset = transform.position - player.position;     
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 마우스의 입력 받기
-        moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        LookAround();
+        float mouseX = Input.GetAxis("Mouse X");
+        cameraTransform.Rotate(0, mouseX * rotationSpeed, 0);
+        transposer.m_FollowOffset = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0) * transposer.m_FollowOffset;
 
-        transform.position = player.position + offset;
-        transform.LookAt(player);
+
+        //// 마우스의 입력 받기
+        //moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //LookAround();
+
+        //transform.position = player.position + offset;
+        //transform.LookAt(player);
     }
 
-    void LookAround()
-    {
-        Vector3 mouseDelta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Vector3 camAngle = CameraArm.rotation.eulerAngles;
+    //void LookAround()
+    //{
+    //    Vector3 mouseDelta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    //    Vector3 camAngle = CameraArm.rotation.eulerAngles;
 
-        float x = camAngle.x - mouseDelta.y;
-        if( x < 180f)
-        {
-            x = Mathf.Clamp(x, -1f, 45f);
-        }
-        else
-        {
-            x = Mathf.Clamp(x, 320f, 361f);
-        }
+    //    float x = camAngle.x - mouseDelta.y;
+    //    if( x < 180f)
+    //    {
+    //        x = Mathf.Clamp(x, -1f, 45f);
+    //    }
+    //    else
+    //    {
+    //        x = Mathf.Clamp(x, 320f, 361f);
+    //    }
 
-        CameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
-    }
+    //    CameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    //}
 }
