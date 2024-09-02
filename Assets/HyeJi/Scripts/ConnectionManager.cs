3639353cb2aa5ca1f99681f997046770957faeab
hzkkong <hz0227798@gmail.com>
2024-09-02 21:12:22 +0900

    로비씬 UI는 일단 완료

---
+++ Assets/HyeJi/Scripts/ConnectionManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
