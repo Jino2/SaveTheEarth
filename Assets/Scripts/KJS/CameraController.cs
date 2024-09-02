256922c3160ae22643968d2109cc937a1ab73976
JSKIM-G <emflazlwm@naver.com>
2024-09-02 15:56:20 +0900

    오류 수정

--- Assets/Scripts/KJS/CameraController.cs
+++ Assets/Scripts/KJS/CameraController.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController_KJS : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
