afeef4c14ae8f19094fd55a868d4f876b6b9f285
hzkkong <hz0227798@gmail.com>
2024-09-04 22:19:38 +0900

    씨네머신 축 조정 / 로그인 화면 UI 완료
    UI 디테일 잡아야함

--- Assets/HyeJi/Scripts/ThirdPersonCamera.cs
+++ Assets/HyeJi/Scripts/ThirdPersonCamera.cs
@@ -27,7 +27,7 @@ public class ThirdPersonCamera : MonoBehaviour
             Debug.LogError("버츄얼카메라내놔");
         }
 
-        currentDistance = maxDistance;
+        //currentDistance = maxDistance;
     }
 
     void Update()
@@ -40,11 +40,11 @@ public class ThirdPersonCamera : MonoBehaviour
         mouseX += Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
         mouseY -= Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
         // 카메라 상하 각도 제한
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        mouseY = Mathf.Clamp(mouseY, 0, 30);
 
-        // RayCasting
 
         virtualCamera.transform.position = player.position;
         virtualCamera.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
     }
 }
