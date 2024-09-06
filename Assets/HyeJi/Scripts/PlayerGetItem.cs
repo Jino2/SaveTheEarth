065f7edc7be5c55111c8cd19b9093da4a6ec5633
JSKIM-G <emflazlwm@naver.com>
2024-09-06 12:47:50 +0900

    플레이어랑 인벤토리 연동

--- Assets/HyeJi/Scripts/PlayerGetItem.cs
+++ Assets/HyeJi/Scripts/PlayerGetItem.cs
@@ -8,13 +8,37 @@ public class PlayerGetItem : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 
-                
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            // 아이템을 인벤토리에 넣자 (인벤 스크립트 참조하면 될듯?)
+            // 아이템의 정보를 가져오기
+            GoodsInfo goodsInfo = other.gameObject.GetComponent<GoodsInfo>();
+
+            if (goodsInfo != null)
+            {
+                // Inventory_KJS에 아이템 추가 (수량 증가)
+                Inventory_KJS.instance.AddGoods(goodsInfo);
+
+                // 오브젝트 비활성화
+                other.gameObject.SetActive(false);
+
+                // 비활성화된 오브젝트를 Inventory_KJS의 리스트에 추가
+                Inventory_KJS.instance.AddGetObject(other.gameObject);
+            }
+
+            //Destroy(other.gameObject);
+
+            print("먹었다!");
+        }
+    }
 }
