bc480bdaab3c61d40c14b2b9c88cd6f99e846af2
hzkkong <hz0227798@gmail.com>
2024-09-23 10:32:37 +0900

    아바타 포톤 연동

--- Assets/HyeJi/Scripts/H_AvatarColorManager.cs
+++ Assets/HyeJi/Scripts/H_AvatarColorManager.cs
@@ -1,32 +1,44 @@
+﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class H_AvatarColorManager : MonoBehaviour
+public class H_AvatarColorManager : MonoBehaviourPun
 {
     public Renderer avatarRenderer;
 
     // Actor Number에 따른 아바타 색상 설정
     public void SetAvatarColor(int actorNumber)
     {
-        Color avatarColor = Color.white;  
+        Color avatarColor = GetColorByActorNumber(actorNumber);
 
+        
+        // 아바타의 Renderer를 통해 색상 변경
+        if (avatarRenderer != null)
+        {
+            avatarRenderer.material.color = avatarColor;
+        }
+
+        // RPC
+        photonView.RPC("RPC_SetAvatarColor", RpcTarget.OthersBuffered, actorNumber);
+    }
+
+    private Color GetColorByActorNumber(int actorNumber)
+    {
         // Actor Number에 따라 색상 결정
         switch (actorNumber)
         {
-            case 1:  // 방장
-                avatarColor = Color.red;
-                break;
-            case 2:  // 1번 플레이어
-                avatarColor = Color.green;
-                break;
-            case 3:  // 2번 플레이어
-                avatarColor = Color.blue;
-                break;
-            default:  // 그 외의 플레이어
-                avatarColor = Color.yellow;
-                break;
+            case 1: return Color.red;
+            case 2: return Color.green;
+            case 3: return Color.blue;
+            default: return Color.yellow;
         }
+    }
+
+    [PunRPC]
+    public void RPC_SetAvatarColor(int actorNumber)
+    {
+        Color avatarColor = GetColorByActorNumber(actorNumber);
 
         // 아바타의 Renderer를 통해 색상 변경
         if (avatarRenderer != null)
